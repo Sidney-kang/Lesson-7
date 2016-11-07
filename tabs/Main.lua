928b6980a7377c5d874e2efb8e365136dc6c310e
sidney-kang at codea-scm <sidney-kang@codea-scm.aws.mapote.com>
2016-11-07 16:21:24 +0000

    Game button

---
+++ tabs/Main.lua
@@ -0,0 +1,24 @@
+-- Lesson 7
+-- Created by:Sidney Kang
+-- Created on: November 4, 2016
+-- Created for: ICS2O
+
+-- Use this function to perform your initial setup
+function setup()
+    print("Hello World!")
+end
+
+-- This function gets called once every frame
+function draw()
+    -- This sets a dark background color 
+    background(40, 40, 50)
+
+    -- This sets the line thickness
+    strokeWidth(5)
+
+    -- Do your drawing here
+    
+    sprite("Project:Blue Forward Button", WIDTH/2, HEIGHT/2, 500, 500)
+    
+end
+
